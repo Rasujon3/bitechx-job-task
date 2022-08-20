424b7e793a373707e7a066714f6a6ec8cd546cc9
Rasujon3 <rasujon3@gmail.com>
2022-08-20 12:22:45 +0600

    medium story done

--- src/Pages/Home/Stories.js
+++ src/Pages/Home/Stories.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Authors from "./Authors";
 import Communities from "./Communities";
+import MediumStory from "./MediumStory";
 import ShortStory from "./ShortStory";
 import Story from "./Story";
 
@@ -14,20 +15,20 @@ const Stories = () => {
   }, []);
   return (
     <div className="">
-      {stories.slice(0, 1).map((story) => (
+      {/* {stories.slice(0, 1).map((story) => (
         <ShortStory key={story.created} story={story}></ShortStory>
       ))}
       {stories.slice(1, 2).map((story) => (
         <Story key={story.created} story={story}></Story>
-      ))}
+      ))} */}
       {stories.slice(2, 3).map((story) => (
-        <Story key={story.created} story={story}></Story>
+        <MediumStory key={story.created} story={story}></MediumStory>
       ))}
-      <Authors />
+      {/* <Authors />
       {stories.slice(3, 6).map((story) => (
         <Story key={story.created} story={story}></Story>
       ))}
-      <Communities />
+      <Communities /> */}
     </div>
   );
 };
